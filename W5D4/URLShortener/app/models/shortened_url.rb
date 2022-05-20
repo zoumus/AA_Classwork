b77c115d25d5973d52b8b4fe6aff45cf0cf645e8
zehour <zehourchaoui@gmail.com>
2022-05-19 18:36:41 -0700

    rails first day

---
+++ W5D4/URLShortener/app/models/shortened_url.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  short_url  :string           not null
+#  long_url   :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+    validates :short_url, :long_url, presence: true, uniqueness: true
+
+    def self.random_code
+        
+    end
+end
